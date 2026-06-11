19064815a74f3df2bf4f0c0f70a035cdcb0a4019
Aaron Summers <ajsbsd@gmail.com>
2026-06-11 11:30:26 -0400

    flake8

--- manage_users.py
+++ manage_users.py
@@ -5,6 +5,7 @@ from werkzeug.security import generate_password_hash
 
 DATABASE = '/home/aaron/www/ajsbsd.db'
 
+
 def get_connection():
     conn = sqlite3.connect(DATABASE)
     conn.row_factory = sqlite3.Row
